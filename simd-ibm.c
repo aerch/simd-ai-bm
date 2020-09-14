4f76a2e83b322ec209052e9945af2838d7f66b1d
Aleksey Erchenko <aleksey.erchenko@gmail.com>
2020-09-14 10:24:09 +0300

    v0.1- update_commit

--- simd-ibm.c
+++ simd-ibm.c
@@ -13,11 +13,11 @@ int main( int argc, char const *argv[] ) {
 	const uint64_t cycles_count = 1000000000;
 
 	printf( "SIMD Instructions Benchmark v0.1\n" );
-	if ( system( "grep 'model name' /proc/cpuinfo | uniq | awk '/^model name/{$1=$2=$3=\"\";print $0}' > proccessor" ) != 0 )
+	if ( system( "grep 'model name' /proc/cpuinfo | uniq | awk '/^model name/{$1=$2=$3=\"\";print $0}' > processor" ) != 0 )
 		perror( "error due system call" );
 	if ( system( "inxi -CfMxx -c 23" ) != 0 )
 		perror( "error due system call" );
-	if ( system( "lscpu >> proccessor" ) != 0 )
+	if ( system( "lscpu >> processor" ) != 0 )
 		perror( "error due system call" );
 	printf("AVX2:\t   ASM INSTR\tINTRINSIC FUNC CALL\t TOTAL EVAL TIME\t PER 1 CYCLE\t OPERAT SPEED\n");
 
