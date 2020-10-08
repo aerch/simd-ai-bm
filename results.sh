751817dbbbf875e647664f578abf180e6a4a0575
Aleksey Erchenko <aleksey.erchenko@gmail.com>
2020-10-08 11:08:34 +0300

    v0.9.12ai-08.10.20 update

--- results.sh
+++ results.sh
@@ -1,8 +1,8 @@
 
-echo -en "\nSIMD Arithmetic Instructions Single-Thread Benchmark\n"
-echo -en "------------------------------------------------------------------\n"
+echo -en "\nSIMD Arithmetic Instructions Single-Thread Benchmark Intercomparison\n"
+echo -en "-------------------------------------------------------------------\n"
 cd bmresults ; grep SIMD-AI-ST-BM * | awk -F:SIMD-AI-ST-BM '{print $NF "\t\t" $1}' | sort -gr; cd ..
 
-echo -en "\nSIMD Arithmetic Instructions Multi-Thread Benchmark\n"
-echo -en "------------------------------------------------------------------\n"
+echo -en "\nSIMD Arithmetic Instructions Multi-Thread Benchmark Intercomparison\n"
+echo -en "-------------------------------------------------------------------\n"
 cd bmresults ; grep SIMD-AI-MT-BM * | awk -F:SIMD-AI-MT-BM '{print $NF "\t\t" $1}' | sort -gr; cd ..; echo
